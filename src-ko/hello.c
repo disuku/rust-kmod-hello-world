8b886f05c85b8b68e026f88139e04a6010742c0e
Chris Smith <chris@hichris.com>
2019-03-13 18:49:43 -0400

    Added character device handler.

--- src-ko/hello.c
+++ src-ko/hello.c
@@ -4,17 +4,33 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 
+MODULE_LICENSE("GPL");
+
 const char* get_output(void);
+
+// netlink.c
+//void netlink_init(void);
+//void netlink_exit(void);
 
 /*void _printk(const char* message) {
   printk(message);
 }*/
 
+// device_control.c
+int init_device_control(void);
+void cleanup_device_control(void);
+
 int init_module(void) {
 	const char* out;
-	printk(KERN_INFO "Hello, World!\n");
+	printk(KERN_INFO "Hello, World!2\n");
 	out = get_output();
 	printk(out);
+
+	//netlink_init();
+	init_device_control();
+
+
+    printk(KERN_INFO "Setup complete!\n");
 
 	/*
 	 * A non 0 return means init_module failed; module can't be loaded.
@@ -23,6 +39,8 @@ int init_module(void) {
 }
 
 void cleanup_module(void) {
+	//netlink_exit();
+	cleanup_device_control();
 	printk(KERN_INFO "Goodbye, World!\n");
 }
 
